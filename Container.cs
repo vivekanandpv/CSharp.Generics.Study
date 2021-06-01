cee337a9e78a38babb0c6c08b836ea929a22b0b3
vivek <vivek@athenaeum.in>
2021-06-01 14:25:56 +0530

    done

---
+++ Container.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Generics.Study
+{
+    //  T here is a type parameter
+    //  T is a conventional letter for this case. It could as well be TResult, V, E, car, ...
+    public class Container<T>
+    {
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace CSharp.Generics.Study
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //  To instantiate, we must provide the type parameter
+            //  a special class Container for string will be emitted by the compiler
+            Container<string> stringContainer = new Study.Container<string>();  
+
+            //  In .NET (C#) we have proper generics implementation
+            //  so, unlike Java, we can have primitives as type parameters
+            Container<int> intContainer = new Study.Container<int>();
         }
     }
 }
